8582d418ad580237c350b2cb6b72cb8791421ab1
wazp <wazpys@gmail.com>
2019-06-05 22:27:59 -0500

    Changed to dotenv-webpack to get server env as well

--- next.config.js
+++ next.config.js
@@ -1,9 +1,21 @@
-const { parsed: localEnv } = require('dotenv').config()
-const webpack = require('webpack')
+require('dotenv').config()
+
+const path = require('path')
+const Dotenv = require('dotenv-webpack')
 
 module.exports = {
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(localEnv))
+  webpack: config => {
+    config.plugins = config.plugins || []
+
+    config.plugins = [
+      ...config.plugins,
+
+      // Read the .env file
+      new Dotenv({
+        path: path.join(__dirname, '.env'),
+        systemvars: true
+      })
+    ]
 
     return config
   }

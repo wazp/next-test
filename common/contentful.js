c5d4a27fe416a96f5a1a949be205760ba6924de8
wazp <wazpys@gmail.com>
2019-06-05 22:34:40 -0500

    since it's a public key I'll just add it, I guess...

--- common/contentful.js
+++ common/contentful.js
@@ -3,22 +3,16 @@ const contentful = require('contentful');
 
 const defaultConfig = {
   CTF_SPACE_ID: process.env.CTF_SPACE_ID,
-  CTF_CDA_TOKEN: process.env.CTF_CDA_TOKEN,
-  CTF_CPA_TOKEN: process.env.CTF_CPA_TOKEN
+  CTF_CDA_TOKEN: process.env.CTF_CDA_TOKEN
 };
 
 module.exports = {
   createClient (config = defaultConfig) {
     const options = {
-      host: 'preview.contentful.com',
+      host: 'cdn.contentful.com',
       space: config.CTF_SPACE_ID,
-      accessToken: config.CTF_CPA_TOKEN
-    };    
-
-    if(process.env.NODE_ENV === 'production') {
-      options.host = 'cdn.contentful.com';
-      options.accessToken = config.CTF_CDA_TOKEN;
-    }
+      accessToken: config.CTF_CDA_TOKEN
+    };
 
     return contentful.createClient(options);
   }
